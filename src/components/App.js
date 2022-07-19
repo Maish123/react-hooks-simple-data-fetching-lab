37034d0b07c08e4d0f1ec5e17d6e5ec4ccf140c9
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-19 18:25:44 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,35 @@
+import React, { useEffect,useState } from "react";
+
+
+
+function App(){
+    const[dogData, setDogData]= useState([])
+    const[fetchData, setFetchData]= useState(false)
+
+    useEffect(()=>{
+        console.log("after")
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp)=>resp.json())
+        .then((respObj)=>{
+            setDogData(respObj.message)
+            setFetchData(true);
+        })
+    },[]);
+
+    console.log("before")
+
+    if(!fetchData){
+        return <p>"Loading..."</p>  
+    }
+
+    return(
+        <img
+        src={dogData}
+        alt="A Random Dog"
+        />
+    )
+};
+
+export default App;
+
 // create your App component here
